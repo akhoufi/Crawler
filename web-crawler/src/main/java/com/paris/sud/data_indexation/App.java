c42be90fcf30069cadad5184638ee36248e83bc7
hadhami <hadhami.mat@gmail.com>
2016-10-25 20:04:31 +0200

    init web crawler project

---
+++ web-crawler/src/main/java/com/paris/sud/data_indexation/App.java
@@ -0,0 +1,13 @@
+package com.paris.sud.data_indexation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
