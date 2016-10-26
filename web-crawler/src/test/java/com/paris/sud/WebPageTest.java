13e388da1e6e6cfa09364cbdd1de5f547d6d4b79
hadhami <hadhami.mat@gmail.com>
2016-10-26 21:33:51 +0200

    remove unused classes

--- web-crawler/src/test/java/com/paris/sud/WebPageTest.java
+++
@@ -1,7 +0,0 @@
-package com.paris.sud;
-
-/**
- * Created by Hadhami on 26/10/2016.
- */
-public class WebPageTest {
-}
---
+++ web-crawler/src/test/java/com/paris/sud/extraction/WebPageTest.java
@@ -0,0 +1,16 @@
+package com.paris.sud.extraction;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Hadhami on 26/10/2016.
+ */
+public class WebPageTest {
+
+    @Test
+    public void testGetContent() throws Exception {
+
+    }
+}
