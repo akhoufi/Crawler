281905e779bff7239b3ddfe422af71b477b86560
hadhami <hadhami.mat@gmail.com>
2016-10-27 12:10:17 +0200

    add PageWriter

--- web-crawler/src/main/java/com/paris/sud/indexation/PageWriter.java
+++ web-crawler/src/main/java/com/paris/sud/indexation/PageWriter.java
@@ -1,7 +1,41 @@
 package com.paris.sud.indexation;
 
+import com.paris.sud.crawler.CrawlerUrl;
+import com.paris.sud.extraction.WebPage;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 /**
  * Created by Hadhami on 26/10/2016.
  */
 public class PageWriter {
+
+    public final String outputPath="/Users/Hadhami/aic/Recherche et extraction d'info/Projet/Files";
+
+    public void saveContent(CrawlerUrl url)  throws Exception {
+        WebPage webpage= new WebPage();
+        String content= webpage.getContent(url);
+        content = url . getNiceText();
+        String title   = url . getTitle();
+        String fileId="1";
+        BufferedWriter contentOutput =
+                new BufferedWriter(
+                        new FileWriter(outputPath + "/download_"+ fileId +".txt"));
+        contentOutput . write(content);
+        contentOutput . flush();
+        contentOutput . close();
+        BufferedWriter titleOutput   =
+                new BufferedWriter(
+                        new FileWriter(outputPath + "/title_"+ fileId +".txt"));
+        titleOutput   . write(title);
+        titleOutput   . flush();
+        titleOutput   . close();
+        BufferedWriter urlOutput     =
+                new BufferedWriter
+                        (new FileWriter(outputPath + "/url_"+ fileId +".txt"));
+        urlOutput     . write(url.getUrlString());
+        urlOutput     . flush();
+        urlOutput     . close();
+    }
 }
