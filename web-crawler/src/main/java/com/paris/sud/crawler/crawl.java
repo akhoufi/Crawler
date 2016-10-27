2146a43944bd84ffd6cd581aa2a09a15d3773c87
hadhami <hadhami.mat@gmail.com>
2016-10-28 00:06:27 +0200

    Crawl URLs from file

---
+++ web-crawler/src/main/java/com/paris/sud/crawler/crawl.java
@@ -0,0 +1,7 @@
+package com.paris.sud.crawler;
+
+/**
+ * Created by Hadhami on 27/10/2016.
+ */
+public class crawl {
+}
---
+++ web-crawler/src/test/java/com/paris/sud/crawler/CrawlTest.java
@@ -0,0 +1,10 @@
+package com.paris.sud.crawler;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Hadhami on 27/10/2016.
+ */
+public class CrawlTest {
+
+}
