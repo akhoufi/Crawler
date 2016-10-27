b02c89d3c41245a9b15cd56a24c3daf99faa0521
hadhami <hadhami.mat@gmail.com>
2016-10-27 11:53:02 +0200

    Text extraction

---
+++ web-crawler/src/test/java/com/paris/sud/indexation/PageWriterTest.java
@@ -0,0 +1,16 @@
+package com.paris.sud.indexation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Hadhami on 27/10/2016.
+ */
+public class PageWriterTest {
+
+    @Test
+    public void testSaveContent() throws Exception {
+
+    }
+}
