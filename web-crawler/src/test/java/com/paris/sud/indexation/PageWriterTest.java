7903772b796bb0e2afc0665535cd8c731f27eeca
hadhami <hadhami.mat@gmail.com>
2016-10-27 12:19:48 +0200

    add PageWriterTest

--- web-crawler/src/test/java/com/paris/sud/indexation/PageWriterTest.java
+++ web-crawler/src/test/java/com/paris/sud/indexation/PageWriterTest.java
@@ -1,5 +1,7 @@
 package com.paris.sud.indexation;
 
+import com.paris.sud.crawler.CrawlerUrl;
+import com.paris.sud.extraction.WebPage;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -11,6 +13,11 @@ public class PageWriterTest {
 
     @Test
     public void testSaveContent() throws Exception {
+        String urlString="http://www.lemonde.fr/";
+        PageWriter writer= new PageWriter();
+        CrawlerUrl url = new CrawlerUrl(urlString);
+        writer.saveContent(url);
+
 
     }
 }
