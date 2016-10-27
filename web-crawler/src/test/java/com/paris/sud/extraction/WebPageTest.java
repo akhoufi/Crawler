f4f7faafb795fe0992e7ab61017fe3074af51555
hadhami <hadhami.mat@gmail.com>
2016-10-27 12:16:23 +0200

    add WebPageTest

--- web-crawler/src/test/java/com/paris/sud/extraction/WebPageTest.java
+++ web-crawler/src/test/java/com/paris/sud/extraction/WebPageTest.java
@@ -1,5 +1,6 @@
 package com.paris.sud.extraction;
 
+import com.paris.sud.crawler.CrawlerUrl;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -11,6 +12,10 @@ public class WebPageTest {
 
     @Test
     public void testGetContent() throws Exception {
-
+        String urlString="http://www.lemonde.fr/";
+        WebPage page= new WebPage();
+        CrawlerUrl url = new CrawlerUrl(urlString);
+        String text = page.getContent(url);
+        assertNotNull(text);
     }
 }
